6168aa53abcc6c3d36b14185f1faba433347219f
Angela Mayhua <amayhuaq@gmail.com>
2023-11-01 12:58:48 -0500

    rename file

---
+++ src/views/Login.js
@@ -0,0 +1,61 @@
+import {auth, provider} from "../firebase"
+import { useEffect, useState } from 'react';
+import { signInWithPopup, signOut } from "firebase/auth";
+
+import EmotionDetector from "../emotionDetector/EmotionDetector";
+
+function Login() {
+    const [userAuthenticated, updateAuth] = useState(null)
+
+    const handleSignIn =()=> {
+        signInWithPopup(auth, provider)
+        .then((data) => {
+            updateAuth(data.user)
+            localStorage.setItem("user", data.user)
+        })
+    }
+
+    const handleSignOut = () => {
+        signOut(auth)
+          .then(() => {
+            updateAuth(null)
+            localStorage.removeItem("user");
+            
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
+
+    useEffect(() => {
+        updateAuth(localStorage.getItem("user"))
+    })
+
+    const SignInButton = (
+        <div style={{ textAlign: 'center', padding: '10px' }}>
+            <button onClick={handleSignIn} style={{ cursor: 'pointer', backgroundColor: 'green', color: 'white', padding: '15px', fontSize: '25px', border: 'none', borderRadius: '10px' }}>
+            Sign In With Google
+            </button>
+        </div>
+    )
+
+    const SignOutButton = (
+        <div style={{ textAlign: 'left', padding: '10px' }}>
+            <button onClick={handleSignOut} style={{ cursor: 'pointer', backgroundColor: 'green', color: 'white', padding: '15px', fontSize: '25px', border: 'none', borderRadius: '10px' }}>
+            LogOut
+            </button>
+        </div>
+    )
+
+    return (
+        <div>
+    
+            {userAuthenticated
+                ? <EmotionDetector signOut={handleSignOut} currentUser={userAuthenticated}/> 
+                : SignInButton                    
+            }
+        </div>
+    );
+}
+
+export default Login;
