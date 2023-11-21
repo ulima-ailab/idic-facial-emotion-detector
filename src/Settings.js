d2e6b126f3733e48230462590260df2772997364
Angela Mayhua <amayhuaq@gmail.com>
2023-11-21 00:24:46 -0500

    Update Settings.js
    Update time to 30 minutes

--- src/Settings.js
+++ src/Settings.js
@@ -3,7 +3,7 @@ export const TEST_WEBAPP_COLLECTION = "Test_WebApp";
 
 // Exported object properties
 export const settings = {
-  sendDataTime: 600000, // num of milliseconds to send data to DB
+  sendDataTime: 1800000, // num of milliseconds to send data to DB
   collectDataTime: 1000, // num of milliseconds to collect data in local
   timeForLocalUpdate: 60  // num of seconds used to recover data for computing and updating local variables of facial data
 };
